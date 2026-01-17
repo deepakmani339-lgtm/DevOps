f2f0cfd13a52acd2545c14a37db7f6fd37e618fd
Apple <apple@Apples-MacBook-Pro.local>
2026-01-17 13:21:21 +0530

    02-conversation

---
+++ Shell-Scripts/02-conversation.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+
+echo "Mahesh:: Hello! What's your name?"
+echo "Prabhas:: I Am Prabhas. Nice to meet you!"
+echo "Mahesh:: Nice to meet you too, Prabhas! How are you doing today?"
+echo "Prabhas:: I'm doing great, thanks for asking! How about you?"
+echo "Mahesh:: I'm doing well, thank you! What do you like to do in your free time?"
+echo "Prabhas:: I enjoy with my friends and family. How about you?"
+echo "Mahesh:: I like to read books and go for hikes. Do you have any favorite books?"
+echo "Prabhas:: Yes, I love reading mystery novels. Do you have any recommendations?"
+echo "Mahesh:: If you like mysteries, you should check out Agatha Christie. She's a fantastic author!"
+echo "Prabhas:: Thanks for the recommendation! I'll definitely check her out."
+echo "Mahesh:: You're welcome! It was great chatting with you, Prabhas."
+echo "Prabhas:: Likewise, Mahesh! Have a great day!"
