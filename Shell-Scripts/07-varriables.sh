c53ba1622472765a4921327a1ad094ba7ee88587
Apple <apple@Apples-MacBook-Pro.local>
2026-01-28 20:53:26 +0530

    varrisables-02

---
+++ Shell-Scripts/07-varriables.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+DATE=$DATE
+echo "Today's date is: $DATE"
+TIME=$TIME
+echo "Current time is: $TIME"
+USER=$USER
+echo "Logged in user is: $USER"
+HOME=$HOME
+echo "Home directory is: $HOME"
+PWD=$PWD
+echo "Present working directory is: $PWD"
