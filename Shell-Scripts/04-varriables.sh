14cf80ac2e0a4018086090e5a3cc65b168159210
Apple <apple@Apples-MacBook-Pro.local>
2026-01-28 20:06:18 +0530

    varrisables-02

---
+++ Shell-Scripts/04-varriables.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+Hero1="$1"
+Hero2="$2"
+
+echo "$Hero1:: Hello! What's your name?"
+echo "$Hero2:: I Am $Hero2. Nice to meet you!"
+echo "$Hero1:: Nice to meet you too, $Hero2! How are you doing today?"
+echo "$Hero2:: I'm doing great, thanks for asking! How about you?"
+echo "$Hero1:: I'm doing well, thank you! What do you like to do in your free time?"
+echo "$Hero2:: I enjoy with my friends and family. How about you?"
+echo "$Hero1:: I like to read books and go for hikes. Do you have any favorite books?"
+echo "$Hero2:: Yes, I love reading mystery novels. Do you have any recommendations?"
+echo "$Hero1:: If you like mysteries, you should check out Agatha Christie. She's a fantastic author!"
+echo "$Hero2:: Thanks for the recommendation! I'll definitely check her out."
+echo "$Hero1:: You're welcome! It was great chatting with you, $Hero2."
+echo "$Hero2:: Likewise, $Hero1! Have a great day!"
