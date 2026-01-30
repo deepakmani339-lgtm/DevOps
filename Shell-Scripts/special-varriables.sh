e861087bd06a9543ea027cd8e41cb1b431596cba
Apple <apple@Apples-MacBook-Pro.local>
2026-01-30 20:38:43 +0530

    data-types

--- Shell-Scripts/special-varriables.sh
+++ Shell-Scripts/special-varriables.sh
@@ -3,3 +3,8 @@
 echo all varriables passed to script are: "$@"
 echo all variables passed to script are: $*
 echo total number of arguments passed to script are: $#
+echo the process id of current script is: $$
+echo the last command exit status is: $?
+echo the script name is: $0
+echo the first argument passed to script is: $1
+echo the second argument passed to script is: $2
