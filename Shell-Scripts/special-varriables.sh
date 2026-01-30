f23fb552a38ede5bcef887c104423e48f3250ab5
Apple <apple@Apples-MacBook-Pro.local>
2026-01-30 20:37:54 +0530

    data-types

--- Shell-Scripts/special-varriables.sh
+++ Shell-Scripts/special-varriables.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-echo all varriables passed to script are: $@
-echo all variables passed to script are: "$*"
+echo all varriables passed to script are: "$@"
+echo all variables passed to script are: $*
 echo total number of arguments passed to script are: $#
